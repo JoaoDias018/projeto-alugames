a254001bed4d6fa425b059717cd19e0fae08439f
João Dias <alvesdias018@gmail.com>
2025-02-28 20:28:45 -0300

    improving the function that changes status

--- js/app.js
+++ js/app.js
@@ -1,20 +1,6 @@
 function alterarStatus(id){
-    switch (id){
-        case 1 :
-            alterarEstilo('dash_one','buttonOne');
-        break;
-        case 2 :
-            alterarEstilo('dash_two','buttonTwo');
-        break;
-        case 3 :
-            alterarEstilo('dash_three','buttonThree');
-        break;
-    }
-}
-
-function alterarEstilo(idDash, idButton){
-    let dash = document.getElementById(idDash);
-    let button = document.getElementById(idButton);
+    let dash = document.getElementById(`dash-${id}`);
+    let button = document.getElementById(`button-${id}`);
     if (dash.classList.toString() == "dashboard__item__img"){
         dash.classList.replace('dashboard__item__img','dashboard__item__img--rented');
         button.classList.replace('dashboard__item__button', 'dashboard__item__button--return');
@@ -24,7 +10,6 @@ function alterarEstilo(idDash, idButton){
             dash.classList.replace('dashboard__item__img--rented','dashboard__item__img');
             button.classList.replace('dashboard__item__button--return', 'dashboard__item__button');
             button.innerHTML = 'Alugar';
-        }
-        
-        }
+        }    
+    }
 }
